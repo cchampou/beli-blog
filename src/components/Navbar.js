cf2764757e621ba14ac223d0957a80366f06fcb6
cchampou <clement@champouillon.com>
2020-09-21 21:14:59 +0200

    home breakdown in progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,14 +9,14 @@ const Navbar = () => {
       <div className="navbar-container">
         <ul className="navbar-list">
           <li><Link to="/">Accueil</Link></li>
-          <li><Link to="about">Qui suis-je</Link></li>
-          <li><Link to="shop">Boutique</Link></li>
+          <li><Link to="/about">Qui suis-je</Link></li>
+          <li><Link to="/shop">Boutique</Link></li>
         </ul>
         <Logo height="150" width="213" />
         <ul className="navbar-list">
-          <li><Link to="blog">Blog</Link></li>
-          <li><Link to="contact">Contact</Link></li>
-          <li><Link to="youtube">Youtube</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/youtube">Youtube</Link></li>
         </ul>
       </div>
     </>
