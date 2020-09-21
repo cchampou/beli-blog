7c112926aef52d242a4aa002665f01d084092c0c
cchampou <clement@champouillon.com>
2020-09-21 20:44:37 +0200

    with navbar and footer

---
+++ src/components/Logo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import logoFile from '../img/logo.jpg'
+
+const Logo = ({ width, height }) => <img src={logoFile} alt="Logo Belinda" height={height} width={width} />;
+
+export default Logo;
